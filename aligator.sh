917a022b4ebf570d6ed6c9994bc7ed154c32c7db
Radek 'Goblin' Pieczonka <goblin@pentex.pl>
2022-02-09 22:19:42 +0100

    update

---
+++ aligator.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i 192.168.231.1, --ask-become-pass aligator.yml
---
+++ jared.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ansible-playbook -u root -i jared.pentex.pl, jared.yml #--ask-become-pass aligator.yml
